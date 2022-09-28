bad83b1ea22d358bb1e005cdfc6ca5219309f1cd
Pavel <83354660+Pahapo@users.noreply.github.com>
2022-09-29 01:12:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,73 +1,62 @@
+const logger = (fun) => console.log(fun);
+
 // ------------- part 6 ----------------
 
-palindrome = word => {
-    let lastLater = word.length - 1;
+const palindrome = (word) => {
+  let lastLater = word.length - 1;
 
-    for (var i = 0; i < word.length; i++) {
-        if (word.charAt(i) !== word.charAt(lastLater))
-            return 0;
+  for (let i = 0; i < word.length; i++) {
+    if (word.charAt(i) !== word.charAt(lastLater)) return 0;
 
-        lastLater--;
+    lastLater--;
 
-        if (word.length % 2 === 0) {
-            if (i === (word.length / 2)) 
-                return 1; 
-        } else {
-            if (i === (word.length / 2) - 0.5) 
-                return 1;
-        }
-    }    
-}
+    if (word.length % 2 === 0) {
+      if (i === word.length / 2) return 1;
+    } else {
+      if (i === word.length / 2 - 0.5) return 1;
+    }
+  }
+};
 
-result1 = name => console.log(palindrome(name));
+logger(palindrome("шалаш"));
+logger(palindrome("анна"));
 
-result1('шалаш');
-result1('анна');
-
-result1('телефон');
-result1('часы');
-
+logger(palindrome("телефон"));
+logger(palindrome("часы"));
 
 // ------------- part 7 ----------------
 
+const min = (a, b) => (a < b ? a : 0);
+const max = (a, y) => (a > y ? a : 0);
 
-min = (a, b) => (a < b ? a : b < a ? b : 'Значения равны!');
-max = (a, y) => (a > y ? a : y > a ? y : 'Значения равны!');
+logger(min(2, 10));
+logger(min(43, 19));
+logger(min(22, 22));
 
-result2 = (a, b) => console.log(min(a, b));
-
-result2(2,10);
-result2(43,19);
-result2(22,22);
-
-result3 = (a, y) => console.log(max(a, y));
-
-result3(2,10);
-result3(43,19);
-result3(22,22);
-
+logger(max(2, 10));
+logger(max(43, 19));
+logger(max(22, 22));
 
 // ------------- part 8 ----------------
 
-
-let arr = [12, 80, 14, 01, 16, 0, 18, 19, 2, 70];
+let arr = [12, 80, 14, 1010, 16, 0, 18, 90, 2, 100];
+let str = [];
 
 for (let i = 0; i < arr.length; i++) {
-    arr[i] += "";
+  arr[i] = arr[i].toString();
 
-    if ((arr[i]).length === 2) {
-        for (let j = 0; j < 2; j++) {
-            
-            if (arr[i].charAt(0) == 0)
-                arr[i] = 'zero' + arr[i].charAt(1);
+  if (arr[i].includes(0)) {
+    str = arr[i].split("");
 
-            if (arr[i].charAt(1) == 0)
-                arr[i] = arr[i].charAt(0) + 'zero';             
-        }
+    for (let j = 0; j < str.length; j++) {
+      if (str[j] == 0) {
+        str[j] = "zero";
+      }
     }
-
-    if (arr[i] == 0) 
-        arr[i] = 'zero';
+    arr[i] = str.join("");
+  } else {
+    arr[i] = Number(arr[i]);
+  }
 }
 
-console.log(arr);
+logger(arr);
