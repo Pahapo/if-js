e49dcd71937cd625b00b488fe3a8dfe0a62032fa
Pavel <83354660+Pahapo@users.noreply.github.com>
2022-12-06 17:32:08 +0300

    Update Students.js

--- scripts/entities/Students.js
+++ scripts/entities/Students.js
@@ -7,9 +7,11 @@ export class Students {
 
   getInfo() {
     const arr = [];
-    this.students.map((item, index) => {
-      arr[index] = `${item.firstName} ${item.lastName} - ${item.courseName}, ${now.getFullYear() - item.admissionYear}`;
-    });
+    this.students
+      .sort((prev, next) => next.admissionYear - prev.admissionYear)
+      .map((item, index) => {
+        arr[index] = `${item.firstName} ${item.lastName} - ${item.courseName}, ${now.getFullYear() - item.admissionYear}`;
+      });
 
     return arr;
   }
