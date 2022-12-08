9aed882f26308821220b5bf0989df382eb659463
Pavel <83354660+Pahapo@users.noreply.github.com>
2022-12-08 22:11:29 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,44 @@
-import { data } from "../js/array.js";
+/* fetch API */
 const cards = document.querySelector(".homes__items");
 
-const cardItems = data
-  .map(({ name, city, country, imageUrl }) => {
-    return `<div class="homes__item">
-    <img class="homes__img" src="${imageUrl}" />
-    <div class="homes__text">
-      <h2>${name}</h2>
-    </div>
-    <div class="homes__location">
-      <h2>${city}, ${country}</h2>
-    </div>
-  </div>`;
+fetch("https://if-student-api.onrender.com/api/hotels/popular", {
+  method: "GET",
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`${response.status} - ${response.statusText}`);
+    }
+    return response.json();
   })
-  .join("");
+  .then((data) => {
+    const cardItems = data
+      .map(({ name, city, country, imageUrl }) => {
+        return `<div class="homes__item swiper-slide">
+      <img class="homes__img" src="${imageUrl}" />
+      <div class="homes__text">
+        <h2>${name}</h2>
+      </div>
+      <div class="homes__location">
+        <h2>${city}, ${country}</h2>
+      </div>
+    </div>`;
+      })
+      .join("");
+    cards.insertAdjacentHTML("afterbegin", cardItems);
+  })
+  .catch((err) => console.error(err));
 
-cards.insertAdjacentHTML("afterbegin", cardItems);
+/* slider SWIPER */
+
+let swiper = new Swiper(".mySwiper", {
+  loop: true, // бесконечный слайдер
+  spaceBetween: 30,
+  slidesPerView: 2, // количество слайдов для показа
+  navigation: {
+    nextEl: ".swiper-button-next",
+  },
+  mousewheel: {
+    // прокрутка мышью с помощью колеса
+    sensitivity: 1,
+  },
+});
